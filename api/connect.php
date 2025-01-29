b9135ec63aa1b9adae240e0f58152cd9f3b50700
Mooyi07 <ljt.villanueva@gmail.com>
2025-01-29 21:31:06 +0800

    Changed in API to apply to all programming

--- api/connect.php
+++ api/connect.php
@@ -7,14 +7,18 @@
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    $sql = "SELECT * FROM `roomstate` WHERE `room` = 'ECL'";
-    $results = $conn->query($sql);
-
     if (!empty($_GET['api'])){
-        if ($_GET['api'] = "crhiz"){
-            while($row = $results->fetch_assoc()) {
-                echo $row['state'];
-              }
+        if ($_GET['api'] = "crhiz" && !empty($_GET['room'])){
+            sqlSelect($_GET['room']);
         }
     }
-    ?>
+
+    function sqlSelect($room){
+        $sql = "SELECT * FROM `roomstate` WHERE `room` = '$room'";
+        $results = $GLOBALS['conn']->query($sql);
+        while($row = $results->fetch_assoc()) {
+            echo $row['state'];
+        }
+    }
+
+?>
