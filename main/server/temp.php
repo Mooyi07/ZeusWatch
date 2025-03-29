00755a56d2738d81047437c7c761293b363ad78d
Mooyi07 <ljt.villanueva@gmail.com>
2025-03-29 11:21:18 +0800

    Cleaned the temperature and humidity code

--- main/server/temp.php
+++ main/server/temp.php
@@ -1,35 +1,17 @@
 <?php 
     include 'connectDB.php';
 
-    $r19 = "SELECT id, humidity, temperature FROM roomtemp WHERE room='R19'";
-    $ecl = "SELECT id, humidity, temperature FROM roomtemp WHERE room ='ECL'";
-    $mtb = "SELECT id, humidity, temperature FROM roomtemp WHERE room ='MTB'";
+    $rooms = ['R19', 'ECL', 'MTB'];
+    $tempHum = [];
 
-    $result_r19 = $conn->query($r19);
-    while($data_r19 = $result_r19->fetch_assoc()){
-        $r19_hum = $data_r19["humidity"];
-        $r19_temp = $data_r19["temperature"];
-    }
-    $result_ecl = $conn->query($ecl);
-    while($data_ecl = $result_ecl->fetch_assoc()){
-        $ecl_hum = $data_ecl["humidity"];
-        $ecl_temp = $data_ecl["temperature"];
-    }
-    $result_mtb = $conn->query($mtb);
-    while($data_mtb = $result_mtb->fetch_assoc()){
-        $mtb_hum = $data_mtb["humidity"];
-        $mtb_temp = $data_mtb["temperature"];
+    foreach ($rooms as $room) {
+        $query = "SELECT humidity, temperature FROM roomtemp WHERE room='$room' LIMIT 1";
+        $result = $conn->query($query);
+        if ($data = $result->fetch_assoc()) {
+            $tempHum[strtolower($room) . 'Hum'] = $data['humidity'];
+            $tempHum[strtolower($room) . 'Temp'] = $data['temperature'];
+        }
     }
 
-    $_REQUEST['tempHum'] = array(
-        'r19Hum' => $r19_hum,
-        'r19Temp'=> $r19_temp,
-        'eclHum' => $ecl_hum,
-        'eclTemp'=> $ecl_temp,
-        'mtbHum' => $mtb_hum,
-        'mtbTemp'=> $mtb_temp
-    );
-
-    echo json_encode($_REQUEST['tempHum']);
-
+    echo json_encode($tempHum);
 ?>
