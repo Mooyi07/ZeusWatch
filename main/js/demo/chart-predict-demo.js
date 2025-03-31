058e55131a2d8af82efcbf8539d8a9de54e178a0
Mooyi07 <ljt.villanueva@gmail.com>
2025-03-31 09:37:30 +0800

    Changes in predict chart

--- main/js/demo/chart-predict-demo.js
+++ main/js/demo/chart-predict-demo.js
@@ -2,29 +2,14 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-function number_format(number, decimals, dec_point, thousands_sep) {
-  // *     example: number_format(1234.56, 2, ',', ' ');
-  // *     return: '1 234,56'
-  number = (number + '').replace(',', '').replace(' ', '');
-  var n = !isFinite(+number) ? 0 : +number,
-    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-    s = '',
-    toFixedFix = function(n, prec) {
-      var k = Math.pow(10, prec);
-      return '' + Math.round(n * k) / k;
-    };
-  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-  if (s[0].length > 3) {
-    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-  }
-  if ((s[1] || '').length < prec) {
-    s[1] = s[1] || '';
-    s[1] += new Array(prec - s[1].length + 1).join('0');
-  }
-  return s.join(dec);
+function number_format(number, decimals = 0, dec_point = '.', thousands_sep = ',') {
+  number = parseFloat(number) || 0;
+  const fixedNumber = number.toFixed(decimals);
+
+  let [integerPart, decimalPart] = fixedNumber.split('.');
+  integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
+
+  return decimalPart ? integerPart + dec_point + decimalPart : integerPart;
 }
 
 var d1 = 0;
