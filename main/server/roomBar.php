4145b78333c4d229e58dc437c0f45066545c8c64
Mooyi07 <ljt.villanueva@gmail.com>
2025-04-03 09:14:38 +0800

    Updated the room Bar

--- main/server/roomBar.php
+++ main/server/roomBar.php
@@ -3,50 +3,37 @@
 include 'connectDB.php';
 include 'consumptions.php';
 
-$d1_val = 0;
-$d2_val = 0;
-$d3_val = 0;
+$d1_val = $d2_val = $d3_val = 0;
 
-$d1 = "SELECT * FROM daily WHERE `room`='ECL' ORDER BY `timestamp` DESC LIMIT 1";
-$resultsd1 = $conn->query($d1);
+// Fetch latest daily consumption for each room
+$d1_query = "SELECT daily FROM daily WHERE room='ECL' ORDER BY timestamp DESC LIMIT 1";
+$d2_query = "SELECT daily FROM daily WHERE room='MTB' ORDER BY timestamp DESC LIMIT 1";
+$d3_query = "SELECT daily FROM daily WHERE room='R19' ORDER BY timestamp DESC LIMIT 1";
 
-$d2 = "SELECT * FROM daily WHERE `room`='MTB' ORDER BY `timestamp` DESC LIMIT 1";
-$resultsd2 = $conn->query($d2);
+$d1_result = $conn->query($d1_query);
+$d2_result = $conn->query($d2_query);
+$d3_result = $conn->query($d3_query);
 
-$d3 = "SELECT * FROM daily WHERE `room`='R19' ORDER BY `timestamp` DESC LIMIT 1";
-$resultsd3 = $conn->query($d3);
-
-if ($resultsd1->num_rows > 0){
-        while($data_d1 = $resultsd1->fetch_assoc()){
-                $d1_val = floatval($data_d1["daily"]);
-        }
-} else {
-        $d1_val = 0;
+if ($d1_result && $d1_result->num_rows > 0) {
+    $d1_val = floatval($d1_result->fetch_assoc()["daily"]);
 }
 
-if ($resultsd2->num_rows > 0){
-        while($data_d2 = $resultsd2->fetch_assoc()){
-                $d2_val = floatval($data_d2["daily"]);
-        }
-} else {
-        $d2_val = 0;
+if ($d2_result && $d2_result->num_rows > 0) {
+    $d2_val = floatval($d2_result->fetch_assoc()["daily"]);
 }
 
-if ($resultsd3->num_rows > 0){
-        while($data_d3 = $resultsd3->fetch_assoc()){
-                $d3_val = floatval($data_d3["daily"]);
-        }
-} else {
-        $d1_val = 0;
+if ($d3_result && $d3_result->num_rows > 0) {
+    $d3_val = floatval($d3_result->fetch_assoc()["daily"]);
 }
 
-$_REQUEST['room'] = array(
-        'r1' => bill($d1_val),
-        'r2' => bill($d2_val),
-        'r3' => bill($d3_val)
+// Prepare response array
+$response = array(
+    'r1' => bill($d1_val),
+    'r2' => bill($d2_val),
+    'r3' => bill($d3_val)
 );
 
-echo json_encode($_REQUEST['room']);
-
+// Return JSON response
+echo json_encode($response);
 
 ?>
